a94d9d73316f51fe461bf91fe15f6ce51ecc4d40
NicolasH-L <nhonglao@gmail.com>
2021-04-07 21:52:00 -0400

    Updated files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D _rigidbody2D;
     private const float SpeedPlayer = 7f;
+    private const float SpeedPlayerJump = 2f;
+    private bool _isPlayerGrounded;
+    private Vector2 _jumping;
 
-    // Start is called before the first frame update
     void Start()
     {
+        _jumping = new Vector2(0.0f, 2f);
+        _isPlayerGrounded = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         var movementPlayerX = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedPlayer;
-        var movementPlayerY = Input.GetAxis("Vertical") * Time.deltaTime * SpeedPlayer;
 
-        if (movementPlayerX != 0 || movementPlayerY > 0)
-            transform.Translate(movementPlayerX, movementPlayerY, 0f);
+        if (movementPlayerX != 0)
+            transform.Translate(movementPlayerX, 0f, 0f);
+
+        if (Input.GetKey("space") && _isPlayerGrounded)
+        {
+            _rigidbody2D.AddForce(_jumping * SpeedPlayerJump, ForceMode2D.Impulse);
+            _isPlayerGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Plateform"))
+        {
+            _isPlayerGrounded = true;
+        }
     }
 }
