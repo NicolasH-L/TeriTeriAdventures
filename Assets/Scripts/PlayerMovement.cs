7084b9934c59ffdc1640dbe5b0d005a7b1137463
NicolasH-L <nhonglao@gmail.com>
2021-04-10 00:01:07 -0400

    Added functionnality for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float forceApplied = -1000f;
     [SerializeField] private Rigidbody2D _rigidbody2D;
     private AudioSource _audioSource;
     private const float SpeedPlayer = 7f;
     private const float JumpHeight = 8f;
     private bool _isPlayerGrounded;
+    private HingeJoint2D _hingeJoint2D;
+    private JointMotor2D _jointMotor2D;
 
     void Start()
     {
+        _hingeJoint2D = GetComponent<HingeJoint2D>();
         _audioSource = GetComponent<AudioSource>();
+        _jointMotor2D = _hingeJoint2D.motor;
     }
 
     void Update()
@@ -27,6 +32,18 @@ public class PlayerMovement : MonoBehaviour
         {
             _rigidbody2D.velocity = new Vector2(0f, JumpHeight);
             _audioSource.Play();
+        }
+
+        if (Input.GetKeyDown("j"))
+        {
+            _jointMotor2D.motorSpeed = forceApplied;
+            _hingeJoint2D.motor = _jointMotor2D;
+        }
+
+        if (Input.GetKeyUp("j"))
+        {
+            _jointMotor2D.motorSpeed = 200;
+            _hingeJoint2D.motor = _jointMotor2D;
         }
     }
 
