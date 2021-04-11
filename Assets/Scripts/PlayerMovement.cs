029975a5f0b54d57d00f311c83f9371484ca78ba
NicolasH-L <nhonglao@gmail.com>
2021-04-11 15:37:40 -0400

    Updated the coroutine for attacking and the colliders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private const float forceApplied = -1000f;
-    [SerializeField] private Rigidbody2D _rigidbody2D;
-    [SerializeField] private GameObject judahCross;
-    private AudioSource _audioSource;
     private const float SpeedPlayer = 7f;
     private const float JumpHeight = 8f;
-    private bool _isPlayerGrounded;
+    private const float ForceAppliedAttacking = -1000f;
+    private const float ForceAppliedRetracting = 1000f;
+    private const float DelayTime = 0.4f;
+    [SerializeField] private Rigidbody2D playerRigidBody2D;
+    [SerializeField] private GameObject judahCross;
+    private AudioSource _audioSource;
     private HingeJoint2D _hingeJoint2D;
     private JointMotor2D _jointMotor2D;
-    private Collider2D judahCollider;
+    private Collider2D _judahCollider;
+    private bool _isPlayerGrounded;
 
     void Start()
     {
         _hingeJoint2D = GetComponent<HingeJoint2D>();
         _audioSource = GetComponent<AudioSource>();
         _jointMotor2D = _hingeJoint2D.motor;
-        judahCollider = judahCross.GetComponent<Collider2D>();
-        judahCollider.enabled = false;
+        _judahCollider = judahCross.GetComponent<Collider2D>();
+        _judahCollider.enabled = false;
     }
 
     void Update()
@@ -34,44 +36,46 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown("space") && _isPlayerGrounded)
         {
-            _rigidbody2D.velocity = new Vector2(0f, JumpHeight);
+            playerRigidBody2D.velocity = new Vector2(0f, JumpHeight);
             _audioSource.Play();
         }
 
-        if (Input.GetKeyDown("j"))
+        if (Input.GetKey("j"))
         {
-            _jointMotor2D.motorSpeed = forceApplied;
+            _jointMotor2D.motorSpeed = ForceAppliedAttacking;
             _hingeJoint2D.motor = _jointMotor2D;
-            judahCollider.enabled = true;
-        }
-
-        if (Input.GetKeyUp("j"))
-        {
-            _jointMotor2D.motorSpeed = 200;
-            _hingeJoint2D.motor = _jointMotor2D;
-            StartCoroutine(delay());
+            _judahCollider.enabled = true;
+            StartCoroutine(Delay());
         }
     }
 
-    private IEnumerator delay()
+    private IEnumerator Delay()
     {
-        yield return new WaitForSeconds(1f);
-        judahCollider.enabled = false;
+        yield return new WaitForSeconds(DelayTime);
+        _jointMotor2D.motorSpeed = ForceAppliedRetracting;
+        _hingeJoint2D.motor = _jointMotor2D;
+        _judahCollider.enabled = false;
     }
 
     private void OnCollisionStay2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Plateform"))
+        switch (other.gameObject.tag)
         {
-            _isPlayerGrounded = true;
+            case "Plateform":
+            case "Obstacle":
+                _isPlayerGrounded = true;
+                break;
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Plateform"))
+        switch (other.gameObject.tag)
         {
-            _isPlayerGrounded = false;
+            case "Plateform":
+            case "Obstacle":
+                _isPlayerGrounded = false;
+                break;
         }
     }
 }
