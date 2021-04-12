6692b5aef7aad2ce2bf471bdb71d9d509b114149
NicolasH-L <nhonglao@gmail.com>
2021-04-12 01:29:16 -0400

    Added a script for the chests to open when triggered by the player

---
+++ Assets/Scripts/Chests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chests : MonoBehaviour
+{
+    [SerializeField] private Sprite chests;
+    private SpriteRenderer _chestsRenderer;
+
+    void Start()
+    {
+        _chestsRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _chestsRenderer.sprite = chests;
+        }
+    }
+}
