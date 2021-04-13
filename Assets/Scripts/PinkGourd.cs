02000ce7dcb3c7ae3ac746d1a4edc5ba44198058
NicolasH-L <nhonglao@gmail.com>
2021-04-13 12:59:57 -0400

    Added OnTriggerEnter2D in the script for the pink gourd

--- Assets/Scripts/PinkGourd.cs
+++ Assets/Scripts/PinkGourd.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PinkGourd : MonoBehaviour
+public class PinkGourd : MonoBehaviour, ItemInterface
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void UseItem()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            print("sdfsdf");
+        }
     }
 }
