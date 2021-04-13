64c1721395af6d63a16d2e328084584a2f52255b
NicolasH-L <nhonglao@gmail.com>
2021-04-13 10:56:37 -0400

    Added a wall to the left of the scene when the game begins. Updated the animation of TeriTeri + made an Animator. Updated prefabs config

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,30 @@ public class PlayerMovement : MonoBehaviour
     private const float SpeedPlayer = 7f;
     private const float JumpHeight = 8f;
     private const float ForceAppliedAttacking = -1000f;
-    private const float ForceAppliedRetracting = 1000f;
+    private const float ForceAppliedRetracting = 950f;
     private const float DelayTime = 0.4f;
+    private const int MaxJump = 2;
     [SerializeField] private Rigidbody2D playerRigidBody2D;
     [SerializeField] private GameObject judahCross;
+    private Animator _animatorPlayer;
+    private PolygonCollider2D _polygonCollider2D;
     private AudioSource _audioSource;
     private HingeJoint2D _hingeJoint2D;
     private JointMotor2D _jointMotor2D;
     private Collider2D _judahCollider;
-    private bool _isPlayerGrounded;
+    private bool _hasAttacked;
+    private int _jumpCounter;
 
     void Start()
     {
+        _animatorPlayer = GetComponent<Animator>();
         _hingeJoint2D = GetComponent<HingeJoint2D>();
         _audioSource = GetComponent<AudioSource>();
-        _jointMotor2D = _hingeJoint2D.motor;
         _judahCollider = judahCross.GetComponent<Collider2D>();
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
+        _jointMotor2D = _hingeJoint2D.motor;
         _judahCollider.enabled = false;
+        _jumpCounter = 0;
     }
 
     void Update()
@@ -32,38 +39,55 @@ public class PlayerMovement : MonoBehaviour
         var movementPlayerX = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedPlayer;
 
         if (movementPlayerX != 0)
+        {
             transform.Translate(movementPlayerX, 0f, 0f);
+            if (Input.GetKey("d"))
+            {
+                _animatorPlayer.SetBool("isMovingToTheRight", true);
+            }
+        }
 
-        if (Input.GetKeyDown("space") && _isPlayerGrounded)
+        if (Input.GetKeyUp("d"))
+        {
+            _animatorPlayer.SetBool("isMovingToTheRight", false);
+        }
+        
+        if (Input.GetKeyDown("space") && _jumpCounter < MaxJump)
         {
             playerRigidBody2D.velocity = new Vector2(0f, JumpHeight);
+            _jumpCounter++;
             _audioSource.Play();
         }
 
-        if (Input.GetKey("j"))
+        //TODO : Fix attacking
+        if (Input.GetKey("j") && !_hasAttacked)
         {
             _jointMotor2D.motorSpeed = ForceAppliedAttacking;
             _hingeJoint2D.motor = _jointMotor2D;
             _judahCollider.enabled = true;
+            _hasAttacked = true;
             StartCoroutine(Delay());
         }
     }
 
+    //TODO : Fix the coroutine when the player is attacking
     private IEnumerator Delay()
     {
         yield return new WaitForSeconds(DelayTime);
         _jointMotor2D.motorSpeed = ForceAppliedRetracting;
         _hingeJoint2D.motor = _jointMotor2D;
         _judahCollider.enabled = false;
+        _hasAttacked = false;
     }
 
-    private void OnCollisionStay2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         switch (other.gameObject.tag)
         {
             case "Plateform":
+            case "Plateform2":
             case "Obstacle":
-                _isPlayerGrounded = true;
+                _jumpCounter = 0;
                 break;
         }
     }
@@ -74,8 +98,13 @@ public class PlayerMovement : MonoBehaviour
         {
             case "Plateform":
             case "Obstacle":
-                _isPlayerGrounded = false;
                 break;
         }
+    }
+
+    //TODO : Callback
+    public void resetJump()
+    {
+        _jumpCounter = 0;
     }
 }
