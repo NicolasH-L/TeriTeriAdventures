4d1b7dac64ae455bc17fb03f649fc38ed1c03c07
NicolasH-L <nhonglao@gmail.com>
2021-04-13 16:00:04 -0400

    Edited the welcome screen and added audio clips

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<AudioClip> listWelcomeBgm;
+    private AudioSource _audioSource;
+
     void Start()
+
     {
-        
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = listWelcomeBgm[Random.Range(0, listWelcomeBgm.Count)];
+        _audioSource.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void StartGame()
+    {
+        _audioSource.Stop();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        _audioSource.Stop();
+        Application.Quit();
     }
 }
