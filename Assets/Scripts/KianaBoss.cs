f5cb68de7a9558eec3f63f4ec0a97c864c6a34c2
NicolasH-L <nhonglao@gmail.com>
2021-04-27 09:26:35 -0400

    Create the script for Kiana boss when shooting cookie bullets

---
+++ Assets/Scripts/KianaBoss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KianaBoss : MonoBehaviour
+{
+
+    [SerializeField] private Transform cookieBullets;
+    private Transform _kianaBoss;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _kianaBoss = GetComponent<Transform>();
+        InvokeRepeating("SpawnBullets", 2f, 3f);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void SpawnBullets()
+    {
+        var pos = new Vector2(_kianaBoss.position.x - 5, _kianaBoss.position.y);
+        Instantiate(cookieBullets, pos, _kianaBoss.rotation);
+    }
+}
