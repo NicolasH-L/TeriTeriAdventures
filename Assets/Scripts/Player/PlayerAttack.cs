398303fd17e78c50a04e9ff27206c30442a102d6
igloosyrup <darckuvina@gmail.com>
2021-05-12 17:03:31 -0400

    added animator conditions to attack script for player

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -9,6 +9,8 @@ public class PlayerAttack : MonoBehaviour
     private AudioSource[] _audioSource;
     private Collider2D _judahCollider;
     [SerializeField] private SpriteRenderer _judahBack;
+    private const float DelayTime = 0.4f;
+
     private Animator _animatorPlayer;
 
 
@@ -22,16 +24,31 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         //TODO : Fix attacking
+
         if (Input.GetKey("j") && !_hasAttacked)
         {
-            _audioSource[SoundEffect2].Play();
-            _judahCollider.enabled = true;
-            _hasAttacked = true;
-            // StartCoroutine(Delay());
+            Attack();
         }
         else if (Input.GetKeyUp("j"))
         {
             _judahBack.enabled = true;
         }
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(DelayTime);
+        // _judahCollider.enabled = false;
+        // _hasAttacked = false;
+    }
+
+    private void Attack()
+    {
+        _animatorPlayer.SetTrigger("Attack");
+        _animatorPlayer.SetBool("isMelee", true);
+        _audioSource[SoundEffect2].Play();
+        _judahCollider.enabled = true;
+        _hasAttacked = true;
+        StartCoroutine(Delay());
     }
 }
