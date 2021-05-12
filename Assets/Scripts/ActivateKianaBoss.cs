405d646e4d2db0605f84cc1645512eea7b40998e
NicolasH-L <nhonglao@gmail.com>
2021-05-12 14:26:31 -0400

    Updated the boss wall when the player is activating the trigger

--- Assets/Scripts/ActivateKianaBoss.cs
+++ Assets/Scripts/ActivateKianaBoss.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 public class ActivateKianaBoss : MonoBehaviour
 {
     [SerializeField] private KianaBoss kianaBoss;
+    [SerializeField] private GameObject blockingWall;
 
     void Start()
     {
+        blockingWall.SetActive(false);
         kianaBoss.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
             kianaBoss.enabled = true;
+            blockingWall.SetActive(true);
+        }
     }
 }
