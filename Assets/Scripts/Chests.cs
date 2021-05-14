7eab4584ac883c11e6716ef2c7365fe6f8e4b015
NicolasH-L <nhonglao@gmail.com>
2021-05-13 23:06:39 -0400

    RNG chests

--- Assets/Scripts/Chests.cs
+++ Assets/Scripts/Chests.cs
@@ -10,35 +10,24 @@ public class Chests : MonoBehaviour
     private const float OffSetX = 0.1f;
     [SerializeField] private Sprite chests;
     [SerializeField] private List<GameObject> _listItems;
-    [SerializeField] private int pinkGourdDropRate;
-    [SerializeField] private int greenGourdDropRate;
-    [SerializeField] private int invincibleGourdDropRate;
-    [SerializeField] private int bandAidDropRate;
-    [SerializeField] private int teriTicketDropRate;
-    [SerializeField] private int potionDropRate;
     private SpriteRenderer _chestsRenderer;
+    private GameObject _itemToSpawn;
 
     void Start()
     {
         _chestsRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
-    {
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            _chestsRenderer.sprite = chests;
-            var index = Random.Range(0, _listItems.Count);
-            var spawnItemPosition = transform.position;
-            spawnItemPosition.y += OffSetY;
-            spawnItemPosition.x -= OffSetX;
-            var randomItems = _listItems[index];
-            Instantiate(randomItems, spawnItemPosition, transform.rotation);
-        }
+        if (!other.gameObject.CompareTag("Player")) return;
+        _chestsRenderer.sprite = chests;
+        var spawnItemPosition = transform.position;
+        spawnItemPosition.y += OffSetY;
+        spawnItemPosition.x -= OffSetX;
+        var index = Random.Range(0, _listItems.Count);
+        var randomItems = _listItems[index];
+        Instantiate(randomItems, spawnItemPosition, transform.rotation);
     }
 
     private void OnTriggerExit2D(Collider2D other)
