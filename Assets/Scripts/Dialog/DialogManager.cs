c52c8fbd0921a8f2aa1a4beeceeb0abce25afc43
igloosyrup <darckuvina@gmail.com>
2021-05-15 18:05:46 -0400

    wip

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -16,11 +16,14 @@ public class DialogManager : MonoBehaviour
     private Canvas _canvasDialogue;
     private Canvas _playerUI;
     private Queue<string> _sentences;
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+    private static readonly int IsEnter = Animator.StringToHash("IsEnter");
 
     private void Start()
     {
         _sentences = new Queue<string>();
-        _playerUI = GameObject.FindGameObjectWithTag(PlayerUiTag).GetComponent<Canvas>();
+        if (GameObject.FindGameObjectWithTag(PlayerUiTag) != null)
+            _playerUI = GameObject.FindGameObjectWithTag(PlayerUiTag).GetComponent<Canvas>();
         _nameText = GameObject.FindGameObjectWithTag(NameTag).GetComponent<TextMeshProUGUI>();
         _dialogueText = GameObject.FindGameObjectWithTag(DialogTag).GetComponent<TextMeshProUGUI>();
         _canvasDialogue = GameObject.FindGameObjectWithTag(CanvasDialogTag).GetComponent<Canvas>();
@@ -28,8 +31,10 @@ public class DialogManager : MonoBehaviour
 
     public void StartDialog(Dialog dialog)
     {
-        _dialogueBox.SetBool("IsOpen", true);
-        _npc.SetBool("IsEnter", true);
+        _dialogueBox.SetBool(IsOpen, true);
+        _npc.SetBool(IsEnter, true);
+        // Enable Line for Testing only.
+        // if (_playerUI != null)
         _playerUI.enabled = false;
         _nameText.text = dialog.name;
         _sentences.Clear();
@@ -55,9 +60,11 @@ public class DialogManager : MonoBehaviour
 
     private void EndDialogue()
     {
-        _dialogueBox.SetBool("IsOpen", false);
-        _npc.SetBool("IsEnter", false);
+        _dialogueBox.SetBool(IsOpen, false);
+        _npc.SetBool(IsEnter, false);
         _canvasDialogue.enabled = false;
+        // Enable Line for Testing only.
+        // if (_playerUI != null)
         _playerUI.enabled = true;
     }
 
